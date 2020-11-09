2070f58c61dc9b66357b1690bb4c90afab6b283b
Teja Chilumula <tejachilumula07@gmail.com>
2020-11-09 11:37:32 +0530

    Negative Number Test

--- src/StringCal.java
+++ src/StringCal.java
@@ -1,6 +1,11 @@
+/*************
+ * Created by Teja Chilumula 11-9-2020
+ */
 
+import java.lang.*;
 public class StringCal {
- public static int Add(String word) {
+ public static int Add(String word) throws Exception  {
+	 //String Greater[]=word.split(",");
 	 int sum=0;
 	 if(word=="")
 	 return 0;
@@ -14,14 +19,60 @@ public class StringCal {
 		 }
 		 return sum;
 	 }
-	 else{
+	 else if(word.indexOf("|\n")==-1){
 		 sum=0;
-			word.replaceAll("\n",",");
-			String numList[] = word.split(",");
+			String numList[] = word.split(",|\n");
 			for(String i : numList ) {
 				 sum+=Integer.parseInt(i);
 			 }
 			return sum;	
 		}
- }
+	
+	 
+	 else {
+		 
+		 //String inputNums[]=word.split(",");
+		 //return summ(inputNums);
+		 String delimiter = ",";
+			if(word.matches("//(.*)\n(.*)")){
+				delimiter = Character.toString(word.charAt(2));
+				word = word.substring(4);
+			}
+
+			String numList[] = word.split( delimiter + "|\n");
+			return summ(numList);
+		 
+         
+		 }
+		 
+	 }
+public static int greater(String word) {
+	String Gre[]=word.split(",");
+	int a=summ(Gre);
+	return a;
+	
 }
+ 
+ public static int summ(String[] word) {
+	 int sum=0;
+	 String Negative="";
+	 for(String i:word) {
+		 if(Integer.parseInt(i)<0) {
+			 if(Negative.equals("")) {
+				 Negative=i;
+			 }
+			 else
+				 Negative+=(","+i);
+		 }
+		 else if(Integer.parseInt(i)<1000) {
+			 
+			 sum+=Integer.parseInt(i);
+		 }
+		 if(!Negative.equals("")) {
+			 throw new IllegalArgumentException("Neagtives not allowed: "+Negative);
+		 }
+ }
+ return sum;
+}
+ }
+	
--- src/StringcalculatorTest.java
+++ src/StringcalculatorTest.java
@@ -1,24 +1,40 @@
 import static org.junit.jupiter.api.Assertions.*;
+import java.lang.*;
 
 import org.junit.jupiter.api.Test;
 
 class StringcalculatorTest {
 //Test case for empty string
 	@Test
-	public void Emptytest() {
+	public void Emptytest() throws Exception {
 		assertEquals(0,StringCal.Add(""));
 	}
 	@Test
-	public void OneNumTest() {
+	public void OneNumTest() throws Exception{
 		assertEquals(1,StringCal.Add("1"));
 	}
 	@Test
-	public void TwoNumTest() {
+	public void TwoNumTest() throws Exception{
 		assertEquals(3,StringCal.Add("1,2"));
 	}
 	@Test
-	public void threeNumTest() {
+	public void threeNumTest()  throws Exception{
 		assertEquals(6,StringCal.Add("1\n2,3"));
 	}
 	
+	
+	@Test
+	//(Expected=IllegalArgumentException.class)
+	public void NegativeNumTest() throws Exception {
+		StringCal calc = new StringCal();
+		
+		try{
+			calc.Add("-1,2");
+			
+		}
+		catch(IllegalArgumentException e) {
+			assertEquals(e.getMessage(),"Negative not allowed: -1");
+		}
+	}
+		
 }
